58e49c411761f9f78fac8bd0f9f752adbc07d466
w77996 <912513404@qq.com>
2018-12-02 23:04:05 +0800

    首页及各个组件编写

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <home-header :city="city"></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons></home-icons>
+    <home-recommend></home-recommend>
+  </div>
+</template>
+<script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
+import axios from 'axios'
+export default {
+  name: 'Home',
+  components: {
+    HomeSwiper,
+    HomeHeader,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
+  },
+  data () {
+    return {
+      city: '',
+      swiperList: []
+    }
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json')
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.city = data.city
+        this.swiperList = data.swiperList
+        console.log(data.city)
+      }
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
+  }
+}
+</script>
+<style>
+</style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="icons">
+    <div class="recommend-title">jjjjj</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend'
+}
+</script>
+<style lang="stylus" scoped>
+  .recommend-title
+    line-height :.8rem
+</style>
